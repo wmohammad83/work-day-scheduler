d967c6fdea58bbce8185d5f8ce5c1a1096c343c4
Waseem Mohammad <wmohammad83@icloud.com>
2023-01-22 13:49:36 +0000

    clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,75 +2,85 @@
 // MOMENT JS SETTING THE DATE
 // =========================================================================
 var today = moment();
-$("#currentDay").text(today.format("dddd Do MMMM YYYY LT"));
+$("#currentDay").text(today.format("dddd Do MMMM YYYY"));
 
 // =========================================================================
 // SELECTING THE CONTAINER
 // =========================================================================
-
-
-// console.log(hour);
 var planner = $("#planner");
-
+// =========================================================================
+// FUNCTION WITH DYNAMIC START AND END TIME
+// =========================================================================
 function dayPlanner(start, end) {
-
-
-    var hour = today.hour();
-
+  var hour = today.hour();
+  // =========================================================================
+  // FOR LOOP TO CREATE TIMEBLOCKS WITH THE BUTTONS AND SAVE FUNCTION
+  // =========================================================================
   for (var i = start; i <= end; i++) {
     var eventTime = moment({
       h: i,
     });
-
-    // new block
+    // =========================================================================
+    // NEW TIMEBLOCK
+    // =========================================================================
     var timeblock = $("<div>");
     timeblock.attr("class", "row time-block");
-
-    // colum
-    var colum = $("<div>");
-    colum.attr("class", "col-md-1 hour");
-    colum.attr("id", "hour-" + i);
-    colum.text(i + ":00");
-
-    // textarea
-    var txt = $("<textarea>");
-    txt.attr("class", "col-md-10 description");
-    txt.attr("id", "task-" + i);
-
-
-    if(hour > i){
-        txt.addClass("past")
-    } else if(hour === i) {
-        txt.addClass("present")
-    } else{
-        txt.addClass("future")
+    // =========================================================================
+    // NEW HOURBLOCK
+    // =========================================================================
+    var hourBlock = $("<div>");
+    hourBlock.attr("class", "col-md-1 hour");
+    hourBlock.attr("id", "hour-" + i);
+    hourBlock.text(i + ":00");
+    // =========================================================================
+    // NEW TEXTAREA INPUT
+    // =========================================================================
+    var textInput = $("<textarea>");
+    textInput.attr("class", "col-md-10 description");
+    textInput.attr("id", "task-" + i);
+    // =========================================================================
+    // SET CLASSES ACCORDING TO CURRENT TIME
+    // =========================================================================
+    if (hour > i) {
+      textInput.addClass("past");
+    } else if (hour === i) {
+      textInput.addClass("present");
+    } else {
+      textInput.addClass("future");
     }
-
-    // Button
+    // =========================================================================
+    // NEW SAVE BUTTON
+    // =========================================================================
     var btn = $("<button>");
     btn.attr("class", "col-md-1 saveBtn");
     btn.attr("id", "btn-" + i);
     var fa = $("<i>");
     fa.attr("class", "fa fa-save");
     btn.append(fa);
-
-
-
-    timeblock.append(colum);
-    timeblock.append(txt);
+    // =========================================================================
+    // APPEND ELEMENTS TO TIMEBLOCK
+    // =========================================================================
+    timeblock.append(hourBlock);
+    timeblock.append(textInput);
     timeblock.append(btn);
     planner.append(timeblock);
-
+    // =========================================================================
+    // EVENT HANDLER TO SAVE THE TASK TO LOCAL STORAGE
+    // =========================================================================
     $(document).on("click", "#btn-" + i, function () {
       var plannerTask = $(this).siblings("textarea").val();
       var taskHour = $(this).siblings("textarea").attr("id").split("-")[1];
-
       localStorage.setItem("task-" + taskHour, plannerTask);
     });
+    // =========================================================================
+    // GET DATA FROM LOCAL STORAGE AND DISPLAY IN BROWSER
+    // =========================================================================
     var hourTask = localStorage.getItem("task-" + i);
     $("#task-" + i).append(hourTask);
   }
 }
 
+// =========================================================================
+// CALL THE PLANNER FUNCTION AND SET THE START AND END TIME OF THE PLANNER
+// =========================================================================
 dayPlanner(9, 17);
-

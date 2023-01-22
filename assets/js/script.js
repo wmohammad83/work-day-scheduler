2be3f842846a85e1144f56ce46aa8716836a738f
Waseem Mohammad <wmohammad83@icloud.com>
2023-01-22 13:31:30 +0000

    jQuery logic added for colour class and event time, also save to local storage set and rerive from local storage to show in browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,15 @@ $("#currentDay").text(today.format("dddd Do MMMM YYYY LT"));
 // SELECTING THE CONTAINER
 // =========================================================================
 
-var hour = today.format();
 
-console.log(hour);
-
-{
-  /* <div class="row time-block">
-        <div class="col-md-1 hour">9am</div>
-        <textarea class="col-md-10 description past"></textarea>
-        <button class="col-md-1 saveBtn"><i class="fa fa-save"></i></button>
-</div> */
-}
-
+// console.log(hour);
 var planner = $("#planner");
 
 function dayPlanner(start, end) {
+
+
+    var hour = today.hour();
+
   for (var i = start; i <= end; i++) {
     var eventTime = moment({
       h: i,
@@ -35,47 +29,48 @@ function dayPlanner(start, end) {
     // colum
     var colum = $("<div>");
     colum.attr("class", "col-md-1 hour");
-    colum.text(i);
+    colum.attr("id", "hour-" + i);
+    colum.text(i + ":00");
 
     // textarea
     var txt = $("<textarea>");
-
     txt.attr("class", "col-md-10 description");
+    txt.attr("id", "task-" + i);
 
-    if (eventTime.isBefore(hour)) {
-      console.log("hello this is from the funtion");
-      txt.attr("class", "past col-md-10 description");
-    }
-    if (eventTime.isSame(hour)) {
-      txt.attr("class", "present col-md-10 description");
-      console.log("time is the same");
-    }
-    if (eventTime.isAfter(hour)) {
-      txt.attr("class", "future col-md-10 description");
+
+    if(hour > i){
+        txt.addClass("past")
+    } else if(hour === i) {
+        txt.addClass("present")
+    } else{
+        txt.addClass("future")
     }
 
+    // Button
     var btn = $("<button>");
     btn.attr("class", "col-md-1 saveBtn");
-
+    btn.attr("id", "btn-" + i);
     var fa = $("<i>");
     fa.attr("class", "fa fa-save");
-
     btn.append(fa);
+
+
 
     timeblock.append(colum);
     timeblock.append(txt);
     timeblock.append(btn);
     planner.append(timeblock);
+
+    $(document).on("click", "#btn-" + i, function () {
+      var plannerTask = $(this).siblings("textarea").val();
+      var taskHour = $(this).siblings("textarea").attr("id").split("-")[1];
+
+      localStorage.setItem("task-" + taskHour, plannerTask);
+    });
+    var hourTask = localStorage.getItem("task-" + i);
+    $("#task-" + i).append(hourTask);
   }
 }
 
-dayPlanner(9, 22);
+dayPlanner(9, 17);
 
-var beginningTime = moment({
-  h: 8,
-});
-var endTime = moment({
-  h: 9,
-});
-
-console.log(beginningTime.isBefore(beginningTime));
